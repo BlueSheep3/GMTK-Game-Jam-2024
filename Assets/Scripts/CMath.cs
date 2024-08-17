1f50d679c04cc11b743238dfdb176fc4ac8a7c68
Per6 <per.daude@t-online.de>
2024-08-17 22:59:08 +0200

    camera

--- Assets/Scripts/CMath.cs
+++ Assets/Scripts/CMath.cs
@@ -16,6 +16,18 @@ static class CMath
 		return rect;
 	}
 
+	public static Rect MovementEncapsulate(this Rect rect, Rect other) {
+		Vector2 size = rect.size;
+		Vector2 otherSize = other.size;
+		if(size.x < otherSize.x) throw new System.Exception("Can't encapsulate a bigger rect in a smaller one");
+		if(size.y < otherSize.y) throw new System.Exception("Can't encapsulate a bigger rect in a smaller one");
+		if(rect.xMin > other.xMin) rect.xMin = other.xMin;
+		else if(rect.xMax < other.xMax) rect.xMax = other.xMax;
+		if(rect.yMin > other.yMin) rect.yMin = other.yMin;
+		else if(rect.yMax < other.yMax) rect.yMax = other.yMax;
+		return rect;
+	}
+
 	public static Rect LimitMinSize(this Rect rect, Vector2 minSize){
 		if(rect.width < minSize.x) {
 			float midX = rect.xMin + rect.width / 2;
@@ -28,6 +40,16 @@ static class CMath
 			rect.yMin = midY - minSize.y / 2;
 		}
 		return rect;
+	}
+
+	public static bool Contains(this Rect rect, Rect other) {
+		if(!rect.Contains(other.position)) return false;
+		if(!rect.Contains(other.position + new Vector2(other.width, other.height))) return false;
+		return true;
+	}
+
+	public static Rect WithScale(this Rect rect, Vector2 scale) {
+		return new Rect(rect.position - scale / 2, scale);
 	}
 
 	public static Vector3 WithZ(this Vector2 vector, float value) {
--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -3,23 +3,38 @@ using UnityEngine;
 
 class GameCamera : MonoBehaviour
 {
-
-
 	void FixedUpdate() {
 		Queue<Shape> shapes = PlacingManager.recentShapes;
 		Camera cam = Camera.main;
 		Vector2 pos = cam.transform.position;
-		Rect minSize = new(pos, new(0, 0));
+		Rect targetRect = new(pos, new(0, 0));
 		foreach(Shape shape in shapes) {
 			Vector2 p = shape.transform.position;
-			if(!minSize.Contains(p)) {
-				minSize = minSize.Encapsulate(p);
+			if(!targetRect.Contains(p)) {
+				targetRect = targetRect.Encapsulate(p);
 			}
 		}
-		minSize = minSize.LimitMinSize(new Vector2(8, 5));
-		Vector2 targetPosition = minSize.center;
+		Rect camRect = GetRect(cam);
+		if(camRect.Contains(targetRect)) return;
+		Vector2 targetSize = targetRect.size;
+		targetSize = Vector2.Max(targetSize, new(5 * cam.aspect, 5));
+		camRect = camRect.WithScale(targetSize);
+		targetRect = camRect.MovementEncapsulate(targetRect);
+
+		// TODO test and add extra to the sides
+
+		Vector2 targetPosition = targetRect.center;
 		cam.transform.position = Vector2.Lerp(cam.transform.position, targetPosition, 0.05f).WithZ(-10f);
-		float targetScale = Mathf.Max(minSize.size.x / 2, minSize.size.y);
+		float targetScale = Mathf.Max(targetRect.size.x / cam.aspect, targetRect.size.y);
 		cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetScale, 0.05f);
+	}
+
+	Rect GetRect(Camera cam) {
+		Vector2 size = GetSize(cam);
+		return new Rect((Vector2)cam.transform.position - size / 2, size);
+	}
+
+	Vector2 GetSize(Camera cam) {
+		return new Vector2(cam.orthographicSize * 2 * cam.aspect, cam.orthographicSize * 2);
 	}
 }
