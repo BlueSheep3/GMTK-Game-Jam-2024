7f787ca8dd5f167c68165195e450672b639d47f2
BlueSheep3 <arne.daude@t-online.de>
2024-08-17 22:22:12 +0200

    in game ui & TMP import

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+class UiManager : MonoBehaviour
+{
+	[SerializeField] TMP_Text scoreText;
+	[SerializeField] Image nextShapeImage;
+
+	// TODO update all the information in the in game UI
+}
