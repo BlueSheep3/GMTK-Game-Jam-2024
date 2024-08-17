ae82cfaff839ccc8c8c27a0d70927da079c2f0f8
BlueSheep3 <arne.daude@t-online.de>
2024-08-17 20:54:53 +0200

    start of placing

--- Assets/Scripts/PlacingManager.cs
+++ Assets/Scripts/PlacingManager.cs
@@ -3,7 +3,28 @@ using UnityEngine;
 class PlacingManager : MonoBehaviour
 {
 	[SerializeField] PreviewShape[] previewShapes;
-	[SerializeField] Shape[] shapes;
 	
 	PreviewShape currentPreviewShape = null;
+
+
+	void Start() {
+		SelectRandomShape();
+	}
+
+	void Update() {
+		Vector2 mousePos = Input.mousePosition;
+		mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+		currentPreviewShape.transform.position = mousePos;
+		
+		if(Input.GetMouseButtonDown(0) && currentPreviewShape.CanBePlacedHere()) {
+			currentPreviewShape.Place();
+		}
+	}
+
+
+	void SelectRandomShape() {
+		// TODO select a random shape based on difficulty
+		PreviewShape shape = previewShapes[Random.Range(0, previewShapes.Length)];
+		currentPreviewShape = Instantiate(shape);
+	}
 }
--- Assets/Scripts/PreviewShape.cs
+++ Assets/Scripts/PreviewShape.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 class PreviewShape : MonoBehaviour
 {
+	[SerializeField] Shape placingShape;
+
+
 	public bool CanBePlacedHere() {
 		// TODO
+		return true;
 	}
 
 	public void Place() {
